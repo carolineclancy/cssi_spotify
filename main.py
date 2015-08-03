c725d86278e44cd07c241882c5f433c3b6fb1694
caroline <demouser@MacBook-Air.local>
2015-08-03 15:33:20 -0400

    working with the spotify api

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ class AddSongs(ndb.Model):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        term="jack+johnson"
-        term_type="artist"
+        term="banana+pancakes"
+        term_type="track"
         spotify_data_source = urlfetch.fetch("https://api.spotify.com/v1/search?q={}&type={}&limit=1".format(term, term_type))
         spotify_json_content = spotify_data_source.content
         parsed_spotify_dictionary = json.loads(spotify_json_content)
@@ -67,3 +67,76 @@ app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/add_song', AddSongHandler)
 ], debug=True)
+
+
+
+
+
+
+
+
+# {
+#   "tracks" : {
+#     "href" : "https://api.spotify.com/v1/search?query=banana+pancakes&offset=1&limit=1&type=track",
+#     "items" : [ {
+#       "album" : {
+#         "album_type" : "album",
+#         "available_markets" : [ "CA", "US" ],
+#         "external_urls" : {
+#           "spotify" : "https://open.spotify.com/album/7tTc46dNdE6GGuiQsssWxo"
+#         },
+#         "href" : "https://api.spotify.com/v1/albums/7tTc46dNdE6GGuiQsssWxo",
+#         "id" : "7tTc46dNdE6GGuiQsssWxo",
+#         "images" : [ {
+#           "height" : 576,
+#           "url" : "https://i.scdn.co/image/72d78924e506cb6fd12ac77c5f4a0e3fa1de6880",
+#           "width" : 640
+#         }, {
+#           "height" : 270,
+#           "url" : "https://i.scdn.co/image/ce6d9d7a12b237eb0ca3a59088f4c8a3d5e94fc3",
+#           "width" : 300
+#         }, {
+#           "height" : 58,
+#           "url" : "https://i.scdn.co/image/1f479656718ae130e95464c4b136fbcb633a992f",
+#           "width" : 64
+#         } ],
+#         "name" : "In Between Dreams",
+#         "type" : "album",
+#         "uri" : "spotify:album:7tTc46dNdE6GGuiQsssWxo"
+#       },
+#       "artists" : [ {
+#         "external_urls" : {
+#           "spotify" : "https://open.spotify.com/artist/3GBPw9NK25X1Wt2OUvOwY3"
+#         },
+#         "href" : "https://api.spotify.com/v1/artists/3GBPw9NK25X1Wt2OUvOwY3",
+#         "id" : "3GBPw9NK25X1Wt2OUvOwY3",
+#         "name" : "Jack Johnson",
+#         "type" : "artist",
+#         "uri" : "spotify:artist:3GBPw9NK25X1Wt2OUvOwY3"
+#       } ],
+#       "available_markets" : [ "CA", "US" ],
+#       "disc_number" : 1,
+#       "duration_ms" : 191906,
+#       "explicit" : false,
+#       "external_ids" : {
+#         "isrc" : "USMC60400032"
+#       },
+#       "external_urls" : {
+#         "spotify" : "https://open.spotify.com/track/451GvHwY99NKV4zdKPRWmv"
+#       },
+#       "href" : "https://api.spotify.com/v1/tracks/451GvHwY99NKV4zdKPRWmv",
+#       "id" : "451GvHwY99NKV4zdKPRWmv",
+#       "name" : "Banana Pancakes",
+#       "popularity" : 74,
+#       "preview_url" : "https://p.scdn.co/mp3-preview/bcda641ba7f9bfcb8a50be1b4260b2fcc17e3f02",
+#       "track_number" : 3,
+#       "type" : "track",
+#       "uri" : "spotify:track:451GvHwY99NKV4zdKPRWmv"
+#     } ],
+#     "limit" : 1,
+#     "next" : "https://api.spotify.com/v1/search?query=banana+pancakes&offset=2&limit=1&type=track",
+#     "offset" : 1,
+#     "previous" : "https://api.spotify.com/v1/search?query=banana+pancakes&offset=0&limit=1&type=track",
+#     "total" : 80
+#   }
+# }
